73aa025e1d366b66ee44682512d135a329fe2d1e
github username <github email address>
2018-01-13 18:40:47 -0500

    more code

--- lib/knit_purl/patterns.rb
+++ lib/knit_purl/patterns.rb
@@ -1,22 +1,21 @@
 class KnitPurl::Patterns
 attr_accessor :name, :price, :brand, :url
 
-  def self.knitting
-    self.scrape_patterns
+@@all = []
+
+  def self.new_from_index_page(p)
+    self.new(
+      ##
+    )
   end
 
-  def self.scrape_patterns
-    patterns = []
-
-    patterns << self.scrape_accessories
-    patterns << self.scrape_hats
-    patterns << self.scrape_scarves
-
-    patterns
+  def initialize
+    @name = name
+    @price = price
+    @brand = brand
+    @url = url
+    @@all << self
   end
 
-  def self.scrape_accessories
-    doc = Nokogiri::HTML(open("https://www.purlsoho.com/patterns/knitting.html?collection_filter=934"))
-    #binding.pry
-  end
+
 end
--- lib/knit_purl/scraper.rb
+++ lib/knit_purl/scraper.rb
@@ -6,6 +6,12 @@ class KnitPurl::Scraper
 
   def scrape_patterns_index
     self.get_page.css
-  end   
+  end
+
+  def make_patterns
+    scrape_patterns_index.each do |p|
+      KnitPurl::Pattern.new_from_index_page(p)
+    end
+  end
 
 end#of class
