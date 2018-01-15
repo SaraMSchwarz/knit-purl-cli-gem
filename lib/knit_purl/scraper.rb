1ac276df93aa23cdbcd58a8df9e354e46359962a
github username <github email address>
2018-01-14 20:35:28 -0500

    updates to scraper

--- lib/knit_purl/scraper.rb
+++ lib/knit_purl/scraper.rb
@@ -1,17 +1,22 @@
 # require 'open-uri'
 # require 'nokogiri'
+require 'pry'
 
 class KnitPurl::Scraper
 
   def self.get_page
     doc = Nokogiri::HTML(open("https://www.loveknitting.com/us/knitting-patterns#?"))
+    binding.pry
     patterns = []
-    name = pattern.css("").attribute("data-name")
-    brand = pattern.css("").attribute("data-brand")
-    price = pattern.css("").attribute("data-price")
+    doc.css(".card").each do |pattern|
+      name = pattern.css(".card span").attribute("data-name")
+      brand = pattern.css("").attribute("data-brand")
+      price = pattern.css("").attribute("data-price")
+
 #store each pattern's name, brand and price in a hash and push each hash into our array.
     patterns << {name: name, brand: brand, price: price}
     patterns
+  end
   end
 
   def scrape_patterns_index
