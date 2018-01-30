fc07c23be877c818b09422bd66355d14cd561dcb
github username <github email address>
2018-01-30 15:56:20 -0500

    changes

--- lib/knit_purl/patterns.rb
+++ lib/knit_purl/patterns.rb
@@ -1,19 +1,25 @@
 class KnitPurl::Patterns
-attr_accessor :name, :price, :brand, :url
+attr_accessor :name, :price
 
 @@all = []
 
-  def self.new_from_index_page(p)
-    self.new(
-      ##
-    )
-  end
+  # def self.new_from_index_page(p)
+  #   self.new(
+  #
+  #   )
+  # end
 
   def initialize
     @name = name
     @price = price
-    @brand = brand
-    @url = url
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
     @@all << self
   end
 
